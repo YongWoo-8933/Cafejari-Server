dd95a737cb6380de5e6c90049c2e0a8d6809f174
YongWoo An <dyddn2015@gmail.com>
2023-08-28 19:16:33 +0900

    [fix] favorite cafe 응답정보 수정

--- user/serializers.py
+++ user/serializers.py
@@ -1,7 +1,7 @@
 
 from rest_framework import serializers
 
-# from cafe.serializers import CafeSerializer
+from cafe.serializers import CafeSerializer
 from user.models import User, Profile, Grade, ProfileImage, NicknameAdjective, NicknameNoun
 
 # 기본 serializer ----------------------------------------------------------------------------
@@ -93,12 +93,12 @@ class PartialUserSerializer(serializers.ModelSerializer):
 class ProfileResponseSerializer(ProfileSerializer):
     grade = GradeResponseSerializer(read_only=True)
     profile_image = ProfileImageResponseSerializer(read_only=True)
-    # favorite_cafe = CafeSerializer(read_only=True, many=True)
+    favorite_cafe = CafeSerializer(read_only=True, many=True)
 
     def to_representation(self, instance):
         self.fields['grade'] = GradeResponseSerializer(read_only=True)
         self.fields['profile_image'] = ProfileImageResponseSerializer(read_only=True)
-        # self.fields['favorite_cafe'] = CafeSerializer(read_only=True, many=True)
+        self.fields['favorite_cafe'] = CafeSerializer(read_only=True, many=True)
         return super(ProfileResponseSerializer, self).to_representation(instance)
 
 
