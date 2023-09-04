37323a0aca5488b6d9ead31ea1e02ce366a611f9
YongWoo An <dyddn2015@gmail.com>
2023-09-04 12:00:18 +0900

    [fix] url 연결 수정

--- cafejari/urls.py
+++ cafejari/urls.py
@@ -22,8 +22,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('', views.index),
-    path('tos/', views.index),
-    path('privacy_policy', views.index),
+    path('tos/', views.tos),
+    path('privacy_policy', views.privacy_policy),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('cafe/', include('cafe.urls')),
