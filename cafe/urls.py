8016fa811e8d1c054e2f247586967c8d19f908e7
YongWoo An <dyddn2015@gmail.com>
2023-09-05 19:29:57 +0900

    [fix] location 로직 수정

--- cafe/urls.py
+++ cafe/urls.py
@@ -4,9 +4,9 @@ from rest_framework import routers
 from cafe.views import CafeViewSet, OccupancyRateUpdateLogViewSet, LocationViewSet
 
 router = routers.DefaultRouter()
+router.register('location', LocationViewSet)
 router.register('', CafeViewSet)
 router.register('occupancy_update_log', OccupancyRateUpdateLogViewSet)
-router.register('location', LocationViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
