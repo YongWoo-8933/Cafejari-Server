a032f622a27c9cdb479a9fdc33f4f6a2c95a4114
YongWoo An <dyddn2015@gmail.com>
2023-10-06 16:57:01 +0900

    [debug] cron 작업 디버깅

--- cafejari/settings.py
+++ cafejari/settings.py
@@ -194,7 +194,8 @@ CRONJOBS = [
     # ('*/30 0-14 * * *', 'cron.congestion.update_congestion_area_cron'), # 매일 30분마다 업데이트
     # ('0 19 * * *', 'cron.item.update_item_list_cron'), # 매일 새벽 4시 업데이트
     ('*/10 0-14 * * *', 'cron.congestion.update_congestion_area_cron'), # 매일 30분마다 업데이트
-    ('48 7 * * *', 'cron.item.update_item_list_cron'), # 매일 새벽 4시 업데이트
+    ('59 7 * * *', 'cron.item.update_item_list_cron'), # 매일 새벽 4시 업데이트
+    ('5 15 * * *', 'cron.daily_activity_stack.clear_daily_activity_stack'), # 매일 자정 5분에 업데이트
 ]
 
 # s3
