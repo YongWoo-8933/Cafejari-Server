f0b9e036814428d40add961d90d1dc97ad17ae41
YongWoo An <dyddn2015@gmail.com>
2023-10-25 16:21:34 +0900

    [debug] activity stack cron 작업 디버깅

--- cafejari/settings.py
+++ cafejari/settings.py
@@ -198,7 +198,7 @@ CRONJOBS = [
     ('*/30 * * * *', 'cron.congestion.update_congestion_area'), # 매일 20분마다 업데이트
     ('0 19 * * *', 'cron.item.update_item_list'), # 매일 새벽 4시 업데이트
     # ('5 15 * * *', 'cron.daily_activity_stack.clear_daily_activity_stack'), # 매일 자정 5분에 업데이트
-    ('* * * * *', 'cron.daily_activity_stack.clear_daily_activity_stack'), # 매일 자정 5분에 업데이트
+    ('* * * * *', 'cron.activity_stack.clear_activity_stack'), # 매일 자정 5분에 업데이트
     ('30 15 * * *', 'cron.cafe_vip.update_cafe_vip'), # 매일 자정 30분에 업데이트
     ('50 15 * * *', 'cron.leaderboard.update_leaders'), # 매일 자정 50분에 업데이트
     ('*/10 0-12 * * *', 'cron.occupancy_sharing.check_sharing_activity'), # 매일 9-21시동안 10분마다 업데이트
